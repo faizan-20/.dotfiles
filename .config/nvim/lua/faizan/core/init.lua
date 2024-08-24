247280ecca97730a9764f3c442ab0577e1db9660
Faizan Ahmad <faizan.ahmad@codenicely.in>
2024-08-24 12:18:52 +0530

    highligh yank fix

--- .config/nvim/lua/faizan/core/init.lua
+++ .config/nvim/lua/faizan/core/init.lua
@@ -1,2 +1,12 @@
 require("faizan.core.options")
 require("faizan.core.keymaps")
+
+-- [[ Highlight on yank ]]
+local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+	group = highlight_group,
+	pattern = "*",
+})
